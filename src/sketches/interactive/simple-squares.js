01cebf48f0865e77067d4c6de05b959219112e1a
Robert Groves <rwgdev@gmail.com>
2025-03-19 14:46:03 -0500

    feat: pause before clearing simple squares

--- src/sketches/interactive/simple-squares.js
+++ src/sketches/interactive/simple-squares.js
@@ -7,12 +7,23 @@ export default ($) => {
     const p5Canvas = $.createCanvas(400, 400);
     p5Canvas.parent('app');
     $.background(0);
+    $.fill(255);
+    $.textAlign($.CENTER, $.CENTER);
+    $.text("Move mouse to draw squares.\nAfter 1000 squares canvas will erase after breif pause.", $.width / 2, $.height / 2);
   }
+
+  let tid = null;
 
   $.draw = () => {
     if (circleCount > 1000) {
-      $.clear();
-      circleCount = 0;
+      if (!tid) {
+        tid = setTimeout(() => {
+          $.clear();
+          circleCount = 0;
+          tid = null;
+        }, 1500);
+      }
+      return;
     }
 
     if (pMouseX !== $.mouseX || pMouseY !== $.mouseY) {
