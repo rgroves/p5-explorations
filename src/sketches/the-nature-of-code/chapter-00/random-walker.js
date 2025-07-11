4441227858fbacacc60c4e236f65a0e74b44a2f2
Robert Groves <rwgdev@gmail.com>
2025-07-11 13:44:06 -0500

    Add 8 way step to random walker

--- src/sketches/the-nature-of-code/chapter-00/random-walker.js
+++ src/sketches/the-nature-of-code/chapter-00/random-walker.js
@@ -1,16 +1,25 @@
 class Walker {
-  constructor(x, y, draw) {
-    this.x = x;
-    this.y = y;
-    this.draw = draw;
+  constructor(p5, color, stepWays = 4) {
+    this.p5 = p5;
+    this.color = color || p5.color(0);
+    this.x = p5.width / 2;
+    this.y = p5.height / 2;
+    if (stepWays === 4) {
+      this.step = this.fourWayStep;
+    } else {
+      this.step = this.eightWayStep;
+    }
   }
 
   show() {
-    this.draw(this.x, this.y);
+    const p5 = this.p5;
+    p5.stroke(this.color);
+    p5.point(this.x, this.y);
   }
 
-  step() {
-    const choice = Math.floor(Math.random() * 4);
+  fourWayStep() {
+    const p5 = this.p5;
+    const choice = p5.floor(p5.random(4));
 
     if (choice === 0) {
       this.x++;
@@ -22,23 +31,31 @@ class Walker {
       this.y--;
     }
   }
+
+  eightWayStep() {
+    const p5 = this.p5;
+    const xStep = p5.floor(p5.random(3)) - 1;  // -1, 0, or 1
+    const yStep = p5.floor(p5.random(3)) - 1;
+    this.x += xStep;
+    this.y += yStep;
+  }
 }
 
 export default (p5) => {
-  let walker;
+  let walker4;
+  let walker8;
 
   p5.setup = () => {
     p5.createCanvas(640, 240);
-    walker = new Walker(p5.width / 2, p5.height / 2, (x, y) => {
-      p5.stroke(0);
-      p5.point(x, y);
-    });
-
     p5.background(240);
+    walker4 = new Walker(p5, p5.color(255, 0, 0), 4);
+    walker8 = new Walker(p5, p5.color(0, 0, 255), 8);
   }
 
   p5.draw = () => {
-    walker.step();
-    walker.show();
+    walker4.step();
+    walker8.step();
+    walker4.show();
+    walker8.show()
   }
 }
